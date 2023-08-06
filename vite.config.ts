3ae299e09ef1e806de947118c50e7aebbe01c713
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-08-06 17:11:28 +0300

    import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-})
+  resolve: {
+    alias: {
+      Src: path.resolve(__dirname, 'src'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Pages: path.resolve(__dirname, 'src/pages')
+    }
+  }
+});
