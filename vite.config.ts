3bab15e3f8615d760f148d77da2a11150de82d72
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-09-16 12:03:13 +0300

    some things

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/solomiya/',
   plugins: [react(), eslint()],
   resolve: {
     alias: {
