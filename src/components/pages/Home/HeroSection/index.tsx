9d34a0e5e9b9ba4389a9fdff59222e5c6f06d5cf
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-10-02 16:13:35 +0300

    fix slideshow rerender

--- src/components/pages/Home/HeroSection/index.tsx
+++ src/components/pages/Home/HeroSection/index.tsx
@@ -1,14 +1,21 @@
+import { useGetIssue } from 'Hooks/useGetIssue';
+
 import { SlideShow } from './SlideShow';
 
-export const HeroSection = () => (
-  <section className="snap-start">
-    <div className="w-full px-6 pt-24">
-      <SlideShow />
-    </div>
-    <div className="mt-6 px-4 text-center font-nice-regular text-3xl">
-      <h2>
-        Solomiya is an artists-run independent magazine exploring and showcasing Ukrainian cultural and artistic scene.
-      </h2>
-    </div>
-  </section>
-);
+export const HeroSection = () => {
+  const issue = useGetIssue();
+
+  return (
+    <section className="snap-start">
+      <div className="w-full px-6 pt-24">
+        <SlideShow key={`slideshow-${issue}`} />
+      </div>
+      <div className="mt-6 px-4 text-center font-nice-regular text-3xl">
+        <h2>
+          Solomiya is an artists-run independent magazine exploring and showcasing Ukrainian cultural and artistic
+          scene.
+        </h2>
+      </div>
+    </section>
+  );
+};
