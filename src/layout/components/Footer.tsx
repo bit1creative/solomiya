ad37181a9f3ba8fdb0a45009d314db6aa7021ee8
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-10-05 19:17:20 +0300

    better footer

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,16 +1,12 @@
 import { Link } from 'react-router-dom';
 
 import { useGetIssue } from 'Hooks/useGetIssue';
-import { useMainColor } from 'Hooks/useTheme';
 
 export const Footer = () => {
-  const mainColor = useMainColor();
   const issue = useGetIssue();
 
   return (
-    <div
-      className={`grid grid-cols-3 gap-x-4 gap-y-2 border-0 border-t-[1.5rem] border-solid border-${mainColor} bg-white text-center font-yuni-black text-2xl`}
-    >
+    <div className={`mt-6 grid grid-cols-3 gap-x-4 gap-y-2 bg-white text-center font-yuni-black text-2xl`}>
       <div className="sr-only bg-main-issue-1" />
       <div className="sr-only bg-main-issue-2" />
       <a href="https://instagram.com/solomiyamag?igshid=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer">
