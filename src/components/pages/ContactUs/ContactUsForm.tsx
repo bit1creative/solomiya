8b260bd20771a8142d55bbd63d69b0b6ac5385ae
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-10-16 00:12:30 +0300

    ContactUsForm

--- src/components/pages/ContactUs/ContactUsForm.tsx
+++ src/components/pages/ContactUs/ContactUsForm.tsx
@@ -8,12 +8,12 @@ export const ContactUsForm = () => {
   const [body, setBody] = useState('');
 
   return (
-    <section className="flex min-h-[calc(100dvh-40px)] flex-col items-center justify-center gap-2 p-4 md:mx-auto md:max-w-[60vw] lg:gap-8">
+    <section className="flex min-h-[calc(100dvh-60px)] flex-col items-center justify-center gap-2 p-4 md:mx-auto md:max-w-[60vw] lg:gap-8 xl:min-h-[calc(100dvh-120px)]">
       <SectionTitle title={'Contact'} />
       <div className="flex w-full flex-col gap-2 xl:gap-4">
         <div className="relative flex h-6">
           <hr className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 border border-dashed border-gray-500" />
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 md:text-xl xl:text-2xl">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 font-nice-regular md:text-xl xl:text-2xl">
             Subject
           </div>
         </div>
@@ -26,7 +26,7 @@ export const ContactUsForm = () => {
       <div className="flex w-full flex-col gap-2 xl:gap-4">
         <div className="relative flex h-6">
           <hr className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 border border-dashed border-gray-500" />
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 md:text-xl xl:text-2xl">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 font-nice-regular md:text-xl xl:text-2xl">
             Message
           </div>
         </div>
@@ -37,7 +37,7 @@ export const ContactUsForm = () => {
         />
       </div>
       <a
-        className="mt-4 w-full rounded-md border border-solid px-4 py-2 text-center md:text-xl xl:py-4 xl:text-2xl"
+        className="mt-4 w-full rounded-md border border-solid px-4 py-2 text-center font-nice-regular transition-all hover:bg-neutral-900 hover:text-neutral-50 md:text-xl xl:py-4 xl:text-2xl"
         href={`mailto:${email}?subject=${subject}&body=${body}`}
       >
         Click to Send an Email
