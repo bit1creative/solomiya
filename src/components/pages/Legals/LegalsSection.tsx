d9e5c39e3440f4e55e587092b2a00e7b8e774c86
solomiyamag <mail@sebastianwells.de>
2023-10-22 12:21:41 +0200

    legals

--- src/components/pages/Legals/LegalsSection.tsx
+++ src/components/pages/Legals/LegalsSection.tsx
@@ -1,1 +1,17 @@
-export const LegalsSection = () => <div className="grid place-items-center">PLAIN TEXT Test Test</div>;
+export const LegalsSection = () => (
+  <div className="mt-4 flex flex-col gap-y-4 font-nice-regular text-lg md:text-xl lg:mt-12 lg:gap-y-12 lg:text-2xl xl:text-3xl">
+    <div>
+      <p className="font-nice-bold">RESPONSIBLE FOR THE CONTENT</p>
+      <p>
+        <br />
+        Solomiya Collective NGO
+        <br />
+        29, Vyshneva street, Tatsenky village, Obukhiv raion, Kyivs&apos;ka oblast
+        <br /> 08700 Ukraine
+        <br />
+        hello@solomiyamag.com
+        <br />
+      </p>
+    </div>
+  </div>
+);
