d0dddac3a422fd5ae7ab572af903812b6acb7f5d
solomiyamag <mail@sebastianwells.de>
2023-10-22 12:30:57 +0200

    desc

--- src/components/pages/PrivacyPolicy/PrivacyPolicyDescription.tsx
+++ src/components/pages/PrivacyPolicy/PrivacyPolicyDescription.tsx
@@ -1,7 +1,7 @@
 export const PrivacyPolicyDescription = () => (
   <div className="font-nice-regular text-lg md:text-xl lg:text-2xl xl:text-3xl">
     <p>
-      Confidentiality and security are paramount values for Soлomiya Collective and, consequently, we assume the
+      Confidentiality and security are paramount values for Solomiya Collective and, consequently, we assume the
       commitment to guarantee the privacy of the User at all times and not to collect unnecessary information. Then, we
       provide you with all the necessary information about our Privacy Policy in relation to the Personal Data we
       collect from you from our website solomiyamag.com.com, explaining:
