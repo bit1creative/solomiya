c8b3ba312258a6e69cbbffb28d3da4e0ac04375b
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-03-08 22:21:15 +0200

    [sm3] - little typo

--- src/components/pages/Home/IssuesSection/issuesImages.data.tsx
+++ src/components/pages/Home/IssuesSection/issuesImages.data.tsx
@@ -455,7 +455,7 @@ export const issuesImagesData: { [k: string]: { [k: string]: IssueImageData } } 
           bodies become interchangeable, serving as resources for the war effort. With questions and photographs,
           Vsevolod Kazarin reflects on the impact of war on individuals and society, raising questions about the nature
           of human existence in times of conflict. This project was realized in collaboration with Kommission
-          Alltagskulturforschung für Westfalen - Graggold Magazin.
+          Alltagskulturforschung für Westfalen - Graugold Magazine.
         </div>
       ),
       title: 'Article 65',
