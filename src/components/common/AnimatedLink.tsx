bfeccb1471f084ef8292dd67ef9672e857732746
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-12-14 14:11:28 +0200

    update Link component

--- src/components/common/AnimatedLink.tsx
+++ src/components/common/AnimatedLink.tsx
@@ -1,56 +1,21 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
-import gsap from 'gsap';
 
 interface AnimatedLinkProps extends LinkProps {
   isReactLink?: boolean;
 }
 
 export const AnimatedLink = ({ isReactLink = true, ...props }: AnimatedLinkProps) => {
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
-  const underlinedRef = useRef<HTMLDivElement | null>(null);
+  const linkRef = useRef<HTMLAnchorElement | null>(null);
 
   const overrideProps = {
     ...props,
-    reloadDocument: !isReactLink
+    reloadDocument: !isReactLink,
+    className: `${
+      props.className || ''
+    } relative bg-gradient-to-r from-black to-black bg-[length:0%_3px] hover:bg-[length:100%_3px] bg-no-repeat bg-bottom transition-all duration-300`,
+    ref: linkRef
   } satisfies LinkProps;
 
-  useEffect(() => {
-    let playAnimation: () => void;
-    let playReverseAnimation: () => void;
-
-    const underlined = underlinedRef.current;
-    const wrapper = wrapperRef.current;
-
-    if (underlined && wrapper) {
-      const animate = gsap.fromTo(
-        underlinedRef.current,
-        { width: 0 },
-        {
-          width: '100%',
-          paused: true
-        }
-      );
-
-      playAnimation = () => animate.play();
-      playReverseAnimation = () => animate.reverse();
-
-      wrapper.addEventListener('mouseenter', playAnimation);
-      wrapper.addEventListener('mouseleave', playReverseAnimation);
-    }
-
-    return () => {
-      if (underlined && wrapper) {
-        wrapper.removeEventListener('mouseenter', playAnimation);
-        wrapper.removeEventListener('mouseleave', playReverseAnimation);
-      }
-    };
-  }, [wrapperRef, underlinedRef]);
-
-  return (
-    <div ref={wrapperRef} className="inline w-full">
-      <Link {...overrideProps} />
-      <div ref={underlinedRef} className="mx-auto block h-1 w-0 bg-black lg:h-3" />
-    </div>
-  );
+  return <Link {...overrideProps}>{props.children}</Link>;
 };
