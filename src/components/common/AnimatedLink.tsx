076ec609bc66e6a78495923a4b90a86e687e5bf8
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-12-14 14:33:46 +0200

    rm ref from automatedlink

--- src/components/common/AnimatedLink.tsx
+++ src/components/common/AnimatedLink.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 
 interface AnimatedLinkProps extends LinkProps {
@@ -6,15 +5,12 @@ interface AnimatedLinkProps extends LinkProps {
 }
 
 export const AnimatedLink = ({ isReactLink = true, ...props }: AnimatedLinkProps) => {
-  const linkRef = useRef<HTMLAnchorElement | null>(null);
-
   const overrideProps = {
     ...props,
     reloadDocument: !isReactLink,
     className: `${
       props.className || ''
-    } relative bg-gradient-to-r from-black to-black bg-[length:0%_3px] hover:bg-[length:100%_3px] bg-no-repeat bg-bottom transition-all duration-300`,
-    ref: linkRef
+    } relative bg-gradient-to-r from-black to-black bg-[length:0%_3px] hover:bg-[length:100%_3px] bg-no-repeat bg-bottom transition-all duration-300`
   } satisfies LinkProps;
 
   return <Link {...overrideProps}>{props.children}</Link>;
