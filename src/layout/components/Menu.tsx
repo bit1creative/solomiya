2ecaf3f34dff0dcbe7b00cee94238e9d0b634864
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-12-25 20:55:18 +0200

    add studio link to menu

--- src/layout/components/Menu.tsx
+++ src/layout/components/Menu.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import gsap from 'gsap';
 import SplitType from 'split-type';
+import { AnimatedLink } from 'Src/components/common/AnimatedLink';
 
 import { useIssueMatch } from 'Hooks/useIssueMatch';
 import { useMainColor } from 'Hooks/useTheme';
@@ -145,7 +146,7 @@ export const Menu = ({ onCloseMenu }: Props) => {
       ref={bgRef}
       className="flex h-full w-full select-none items-center justify-center overflow-hidden px-2 lg:block lg:p-4 lg:pb-0"
     >
-      <div className="grid h-min w-full max-w-limit justify-end text-end font-yuni-black text-menu lg:h-full lg:grid-cols-5 lg:text-center lg:text-menu-lg xl:mx-auto xl:text-menu-xl">
+      <div className="grid h-min w-full max-w-limit justify-end text-end font-yuni-black text-menu lg:h-full lg:grid-cols-6 lg:text-center lg:text-menu-lg xl:mx-auto xl:text-menu-xl">
         <Link
           ref={(el) => menuItemsRef.current.push(el as HTMLElement)}
           className="block h-max lg:-scale-100 lg:[writing-mode:vertical-rl]"
@@ -162,6 +163,17 @@ export const Menu = ({ onCloseMenu }: Props) => {
           onClick={onCloseMenu}
         >
           about
+        </Link>
+
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          ref={(el) => menuItemsRef.current.push(el as HTMLElement)}
+          className="block h-max lg:-scale-100 lg:[writing-mode:vertical-rl]"
+          to="https://solomiyastudio.com"
+          onClick={onCloseMenu}
+        >
+          studio
         </Link>
 
         <Link
